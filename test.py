61f75b2ca78d93746b667f08187e4224de6d1c28
Peter Rose <pwrose@ucsd.edu>
2018-10-05 15:31:03 -0700

    setting up CI

---
+++ test.py
@@ -0,0 +1,15 @@
+import subprocess
+import tempfile
+
+
+def _exec_notebook(path):
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        print(fout.name)
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=1000",
+                "--output", fout.name, path]
+        subprocess.check_call(args)
+
+
+def test():
+    _exec_notebook('./example1/0-Workflow.ipynb')
